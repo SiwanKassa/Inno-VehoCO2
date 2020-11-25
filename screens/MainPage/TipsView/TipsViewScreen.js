0753d793e661d8e58e8d5ee7a7f511e45bb5321e
visaso <32449867+visaso@users.noreply.github.com>
2020-11-25 14:08:02 +0200

    Tip view styling

--- screens/MainPage/TipsView/TipsViewScreen.js
+++ screens/MainPage/TipsView/TipsViewScreen.js
@@ -1,41 +1,48 @@
-import * as React from 'react';
+import * as React from "react";
 import styles from "./TipsViewStyle";
-import tips from './Tips/tips';
-import plane from './Images/plane.jpg';
-import pedals from './Images/pedals.jpg';
+import tips from "./Tips/tips";
+import plane from "./Images/plane.jpg";
+import pedals from "./Images/pedals.jpg";
+import VehoButton from "./../../../components/VehoButton";
+import VehoColors from "./../../VehoColors";
 
-import { Text, View, Image, Button } from 'react-native';
+import { Text, View, Image, Button } from "react-native";
 
-function TipsViewScreen({navigation}) {
+function TipsViewScreen({ navigation }) {
   const [tip, onChangeTip] = React.useState(tips.plane);
   const [isPlane, onChangePhoto] = React.useState(true);
 
-  const planePhoto = <Image source={plane} style={styles.image} resizeMode='contain'></Image>
-  const pedalPhoto = <Image source={pedals} style={styles.image}></Image>
+  const planePhoto = (
+    <Image source={plane} style={styles.image} resizeMode="contain"></Image>
+  );
+  const pedalPhoto = <Image source={pedals} style={styles.image}></Image>;
 
-  const planeTip = <Text style={styles.tipText}>{tips.plane.text}</Text>
-  const pedalTip = <Text style={styles.tipText}>{tips.pedals.text}</Text>
+  const planeTip = <Text style={styles.tipText}>{tips.plane.text}</Text>;
+  const pedalTip = <Text style={styles.tipText}>{tips.pedals.text}</Text>;
 
   const onNextClick = () => {
-    onChangePhoto(!isPlane)
-  }
+    onChangePhoto(!isPlane);
+  };
 
   return (
-      /*screen for tips view*/
+    /*screen for tips view*/
     <View style={styles.container}>
-      
-
-      <Text style={styles.title}>Tips to lower your daily carbon footprint</Text>
-
+      <Text style={styles.title}>
+        Tips to lower your daily carbon footprint
+      </Text>
 
       <View style={styles.photoContainer}>
         {isPlane ? planePhoto : pedalPhoto}
       </View>
-        
+
       {isPlane ? planeTip : pedalTip}
 
-
-      <Button title="Next" onPress={onNextClick}></Button>
+      <VehoButton
+        text="Next tip"
+        color={VehoColors.secondary}
+        onPress={onNextClick}
+        width="40%"
+      />
     </View>
   );
 }
